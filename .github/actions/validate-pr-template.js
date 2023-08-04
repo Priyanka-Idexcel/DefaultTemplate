c2797ba4b30a9dd9247faac3194c2048d62729ee
Priyanka k <priyanka.k@idexcel.com>
2023-08-04 18:27:25 +0530

    test

--- .github/actions/validate-pr-template.js
+++ .github/actions/validate-pr-template.js
@@ -1,1 +1,14 @@
+// .github/actions/validate-pr-template.js
+const core = require('@actions/core');
 
+try {
+  const prTemplate = context.payload.pull_request.body;
+  const requiredFields = ['Title', 'Description', 'Related', 'Issues', 'Checklist'];
+
+  const missingFields = requiredFields.filter(field => !prTemplate.includes(field));
+  if (missingFields.length > 0) {
+    core.setFailed(`Missing fields in PR template: ${missingFields.join(', ')}`);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
