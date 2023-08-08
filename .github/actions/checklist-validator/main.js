26bd32b3c50c73100c37216cd36472c4725806cf
Priyanka k <priyanka.k@idexcel.com>
2023-08-08 16:19:52 +0530

    check

--- .github/actions/checklist-validator/main.js
+++ .github/actions/checklist-validator/main.js
@@ -1,4 +1,23 @@
-// cconst fs = require('fs');
+// const fs = require('fs');
+// const path = require('path');
+
+// const prTemplatePath = path.join(process.env.GITHUB_WORKSPACE, '.github', 'PULL_REQUEST_TEMPLATE.md');
+// const prTemplateContent = fs.readFileSync(prTemplatePath, 'utf-8');
+
+// const checkboxPattern = /^\s*-\s*\[x\]/gm;
+// const checkboxMatches = prTemplateContent.match(checkboxPattern);
+
+// if checkboxMatches.length === 0 {
+//   console.error('error: PR checklist is missing. Please add checkboxes to your template.');
+//   process.exit(1);
+// } else if (checkboxMatches.length < requiredCheckboxCount) {
+//   console.error('error: PR checklist is incomplete. Please mark all checkboxes.');
+//   process.exit(1);
+// } else {
+//   console.log('PR checklist is complete.');
+// }
+
+const fs = require('fs');
 const path = require('path');
 
 const prTemplatePath = path.join(process.env.GITHUB_WORKSPACE, '.github', 'PULL_REQUEST_TEMPLATE.md');
@@ -19,3 +38,4 @@ if (checkboxMatches.length < requiredCheckboxCount) {
 }
 
 console.log('PR checklist is complete.');
+
