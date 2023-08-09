873f9c4e9d21b1b83815222e6c0f1b7607c41884
Priyanka k <priyanka.k@idexcel.com>
2023-08-09 14:55:42 +0530

    test

--- .github/actions/checklist-validator/main.js
+++ .github/actions/checklist-validator/main.js
@@ -1,21 +1,21 @@
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// const prTemplatePath = path.join(process.env.GITHUB_WORKSPACE, '.github', 'PULL_REQUEST_TEMPLATE.md');
-// const prTemplateContent = fs.readFileSync(prTemplatePath, 'utf-8');
+const prTemplatePath = path.join(process.env.GITHUB_WORKSPACE, '.github', 'PULL_REQUEST_TEMPLATE.md');
+const prTemplateContent = fs.readFileSync(prTemplatePath, 'utf-8');
 
-// const checkboxPattern = /^\s*-\s*\[x\]/gm;
-// const checkboxMatches = prTemplateContent.match(checkboxPattern);
+const checkboxPattern = /^\s*-\s*\[x\]/gm;
+const checkboxMatches = prTemplateContent.match(checkboxPattern);
 
-// if checkboxMatches.length === 0 {
-//   console.error('error: PR checklist is missing. Please add checkboxes to your template.');
-//   process.exit(1);
-// } else if (checkboxMatches.length < requiredCheckboxCount) {
-//   console.error('error: PR checklist is incomplete. Please mark all checkboxes.');
-//   process.exit(1);
-// } else {
-//   console.log('PR checklist is complete.');
-// }
+if checkboxMatches.length === 0 {
+  console.error('error: PR checklist is missing. Please add checkboxes to your template.');
+  process.exit(1);
+} else if (checkboxMatches.length < requiredCheckboxCount) {
+  console.error('error: PR checklist is incomplete. Please mark all checkboxes.');
+  process.exit(1);
+} else {
+  console.log('PR checklist is complete.');
+}
 
 // const fs = require('fs');
 // const path = require('path');
--- .github/scripts/checklist-validator.rb
+++ .github/scripts/checklist-validator.rb
@@ -1,17 +1,10 @@
 require 'rubygems'
 require 'bundler/setup'
-
 require 'fileutils'
 require 'octokit'
 require 'pry'
 
-# Your code here
-
-# This will start the interactive console
-
-
-# begin
-	ENV["GITHUB_WORKSPACE"] = "/home/ubuntu/Projects/testing/DefaultTemplate/"
+ENV["GITHUB_WORKSPACE"] = "/home/ubuntu/Projects/testing/DefaultTemplate/"
 
 pr_template_path = File.join(ENV["GITHUB_WORKSPACE"], '.github', 'PULL_REQUEST_TEMPLATE.md')
   # pr_template_path = '.github/PULL_REQUEST_TEMPLATE.md'
