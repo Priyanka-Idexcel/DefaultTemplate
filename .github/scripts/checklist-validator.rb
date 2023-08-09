74b106cfad958825caa90a1ff6c8d30a92c9cbd3
Priyanka k <priyanka.k@idexcel.com>
2023-08-09 11:53:19 +0530

    test

--- .github/scripts/checklist-validator.rb
+++ .github/scripts/checklist-validator.rb
@@ -1,26 +1,39 @@
+require 'rubygems'
+require 'bundler/setup'
 
 require 'fileutils'
 require 'octokit'
+require 'pry'
 
-begin
-  pr_template_path = '.github/PULL_REQUEST_TEMPLATE.md'
+# Your code here
+
+# This will start the interactive console
+
+
+# begin
+	ENV["GITHUB_WORKSPACE"] = "/home/ubuntu/Projects/testing/DefaultTemplate/"
+
+pr_template_path = File.join(ENV["GITHUB_WORKSPACE"], '.github', 'PULL_REQUEST_TEMPLATE.md')
+  # pr_template_path = '.github/PULL_REQUEST_TEMPLATE.md'
   pr_template_content = File.read(pr_template_path, encoding: 'utf-8')
+# binding.pry
+  # checkbox_pattern = /^- \[x\]/
+  checkbox_pattern = pr_template_content.include?('[ ]')
+  checkbox_matches = pr_template_content.scan('[ ]')
 
-  checkbox_pattern = /^- \[x\]/
-  checkbox_matches = pr_template_content.scan(checkbox_pattern)
+   required_checkbox_count = checkbox_matches
 
-  required_checkbox_count = # specify the required checkbox count here
-
-  if checkbox_matches.empty?
+  if pr_template_content.include?('[ ]')
     puts 'error: PR checklist is missing. Please add checkboxes to your template.'
-    exit 1
-  elsif checkbox_matches.length < required_checkbox_count
-    puts 'error: PR checklist is incomplete. Please mark all checkboxes.'
-    exit 1
+ 
+  # else checkbox_matches.length < required_checkbox_count
+  #   puts 'error: PR checklist is incomplete. Please mark all checkboxes.'
+  #   exit 1
   else
     puts 'PR checklist is complete.'
   end
-rescue StandardError => error
-  puts "An error occurred: #{error.message}"
-  exit 1
-end
+# end
+# rescue StandardError => error
+#   puts "An error occurred: #{error.message}"
+#   exit 1
+# end
