5e3690dfeaedb8f941d3f5d0edfe057225c3bb9c
Priyanka k <priyanka.k@idexcel.com>
2023-08-09 12:37:05 +0530

    test

--- .github/actions/checklist-validator/main.js
+++ .github/actions/checklist-validator/main.js
@@ -17,26 +17,26 @@
 //   console.log('PR checklist is complete.');
 // }
 
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// process.env["GITHUB_WORKSPACE"]="/home/ubuntu/Projects/testing/DefaultTemplate/";
+process.env["GITHUB_WORKSPACE"]="/home/ubuntu/Projects/testing/DefaultTemplate/";
 
-// const prTemplatePath = path.join(process.env.GITHUB_WORKSPACE, '.github', 'PULL_REQUEST_TEMPLATE.md');
-// const prTemplateContent = fs.readFileSync(prTemplatePath, 'utf-8');
+const prTemplatePath = path.join(process.env.GITHUB_WORKSPACE, '.github', 'PULL_REQUEST_TEMPLATE.md');
+const prTemplateContent = fs.readFileSync(prTemplatePath, 'utf-8');
 
-// const checkboxPattern = /^\s*-\s*\[x\]/gm;
-// const checkboxMatches = prTemplateContent.match(checkboxPattern);
+const checkboxPattern = /^\s*-\s*\[x\]/gm;
+const checkboxMatches = prTemplateContent.match(checkboxPattern);
 
-// if (!checkboxMatches)
-//   console.error('error: No checkboxes found in the PR Template')
-//  process.exit(1)
+if (!checkboxMatches)
+  console.error('error: No checkboxes found in the PR Template')
+ process.exit(1)
 
-// const requiredCheckboxCount =  0
-// if (checkboxMatches.length < requiredCheckboxCount) {
-//   console.error('error: PR checklist is incomplete. Please mark all checkboxes.');
-//   process.exit(1);
-// }
+const requiredCheckboxCount =  0
+if (checkboxMatches.length < requiredCheckboxCount) {
+  console.error('error: PR checklist is incomplete. Please mark all checkboxes.');
+  process.exit(1);
+}
 
-// console.log('PR checklist is complete.');
+console.log('PR checklist is complete.');
 
